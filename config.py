bac55beddba947021b3840ff5ef82ebf381bdc51
root <root@srv1041791.hstgr.cloud>
2025-11-07 13:26:02 +0000

    Enable debug mode

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
+    DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, 'instance', 'uploads')
-
--- run.py
+++ run.py
@@ -8,4 +8,4 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Role': Role}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7500)
+    app.run(host='0.0.0.0', port=7500, debug=app.config.get('DEBUG', False))
